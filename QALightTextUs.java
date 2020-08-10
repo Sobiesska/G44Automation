b9e395b91976e5dd3f1651e3f2a2a89ee1448571
Lilia Podolska <60666430+Sobiesska@users.noreply.github.com>
2020-08-11 00:21:25 +0300

    Add files via upload

---
+++ QALightTextUs.java
@@ -0,0 +1,7 @@
+package someMain;
+
+public class QALightTextUs {
+        String openMessage = "//jdiv[@class=\"hoverl_be6\"]"; //проверить раскрывается ли текстовый бокс по клику
+        String textMessage = "//textarea[@class=\"inputField_a40\"]"; //проверить можно ли написать сообщение
+        String sendMessage = "//jdiv[@class=\"sendButton_825\"]"; //проверить отправляется ли сообщение
+}
